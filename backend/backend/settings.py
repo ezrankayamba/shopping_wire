35dbac95944007a2daaf725492122995a481f8f0
ezrankayamba <ezrankayamba@gmail.com>
2020-12-26 14:54:19 +0300

    Static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images).
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
